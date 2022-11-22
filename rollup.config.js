02c68e174deaf7fa9ed5cac6997454ceba5a6a8c
Hikmet Mehdi-zade <mehdi-zade@firecode.ru>
2022-11-22 17:11:16 +0300

    Install parcel

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,49 @@
-import { readFileSync } from "node:fs";
+import auto from "@rollup/plugin-auto-install";
+
 import dts from "rollup-plugin-dts";
 import pkg from "./package.json" assert { type: "json" };
 import commonJs from "rollup-plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import nodeResolve from "rollup-plugin-node-resolve";
-import flatDts from 'rollup-plugin-flat-dts';
-
+import json from "@rollup/plugin-json";
+import sucrase from "@rollup/plugin-sucrase";
+import esbild from "rollup-plugin-esbuild";
 
 /**
- * @type {import('rollup').RollupOptions}
+ * @type {import('rollup').RollupOptions[]}
  */
-const config = {
-  input: "./index.ts",
-  output: [
-    {
-      file: "./dist/index.js",
-      sourcemap: true,
-      format: "cjs",
-    },
-    {
-      file: "./dist/index.d.ts",
-      sourcemap: true,
-      format: "esm",
-
-      plugins: [flatDts()],
-    },
-  ],
-  plugins: [
-    nodeResolve(),
-    commonJs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
-    dts(),
-  ],
-  external: Object.keys(pkg.dependencies),
-};
+const config = [
+  {
+    input: "./index.js",
+    output: [
+      {
+        file: "./build/index.js",
+        sourcemap: true,
+        format: "commonjs",
+      },
+    ],
+    plugins: [
+      nodeResolve({
+        extensions: [".js", ".ts", ".d.ts"],
+        browser: true,
+        preferBuiltins: true,
+      }),
+      sucrase({
+        exclude: ["node_modules/**"],
+        transforms: ["typescript"],
+      }),
+      commonJs(),
+      json({
+        include: ['package.json']
+      }),
+      esbild({
+        target: "es2020",
+        loaders: {
+          ".json": "json",
+        },
+      }),
+    ],
+    external: Object.keys(pkg.dependencies),
+  },
+];
 export default config;
